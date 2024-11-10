b17363ce78f44317e8d47e1d01bfb934e46e2265
kalyan <kalyanrao2k24@gmail.com>
2024-11-10 18:39:45 +0530

    mernfrontend

--- src/Components/BaseUrl.js
+++ src/Components/BaseUrl.js
@@ -1,3 +1,3 @@
 export const baseUrl="http://localhost:3000"
-export const cloudinaryName="dtjajtwod"
-export const uploadFile="swiggy_upload"
+export const cloudinaryName="dtjajtooo"
+export const uploadFile="upload"
